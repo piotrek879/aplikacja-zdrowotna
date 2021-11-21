81dfa4ee4d34a840040c397dd6bd6985611cc8fe
arekh22 <arekh332@gmail.com>
2021-11-21 14:29:57 +0100

    V3.1

--- App3.Android/MainActivity.cs
+++ App3.Android/MainActivity.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4f8cdbc55ff6d4e2657fcb718c6c8a43353281dfd42319d393fc9c54bf464b0
-size 1225
+oid sha256:c9d0e2dcf8d5cd159bb6c659777371b0c69a017397fd5a0fadc000e2d3f09cfb
+size 2763
--- App3/Ustawienia.xaml.cs
+++ App3/Ustawienia.xaml.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98c1cb63efc22d02ea2ed80c3b4b77c225259c03ab1474fde8e8eb4318628d6a
-size 412
+oid sha256:dab1f2b5db1179260c22e9fb7c8db5315a03a522387079945d2c618697ec2cfc
+size 1535
