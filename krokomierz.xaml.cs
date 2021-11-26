ab72fb8f1fda9849e4223a60feef19c347966a05
arekh22 <92223307+arekh22@users.noreply.github.com>
2021-11-26 15:20:52 +0100

    V4.2
    V4.2

--- krokomierz.xaml.cs
+++ krokomierz.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -13,21 +14,22 @@ namespace App3
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class krokomierz : ContentPage
     {
-        public object LabelX { get; private set; }
+        private double wartoscPoprzednia = 0;
+        private int stepCount = 0;
 
         public krokomierz()
         {
             InitializeComponent();
         }
 
-        private void start_Clicked(object sender, EventArgs e)
+        private void Start_Clicked(object sender, EventArgs e)
         {
             if (Accelerometer.IsMonitoring)
                 return;
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
             Accelerometer.Start(SensorSpeed.UI);
 
-            
+       
 
         }
 
@@ -36,12 +38,48 @@ namespace App3
             throw new NotImplementedException();
         }
 
-        private void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        private async void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
         {
-            
+
+            //while (true)
+            //{
+
+                //opoznienie pomiedzy krokami
+                await Task.Delay(500);
+
+                double x = e.Reading.Acceleration.X;
+                double y = e.Reading.Acceleration.Y;
+                double z = e.Reading.Acceleration.Z;
+
+                //opoznienie pomiedzy krokami
+                await Task.Delay(500);
+
+                double wektorPrzyspieszenia = Math.Sqrt((x * x) + (y * y) + (z * z));
+                double delta = (wektorPrzyspieszenia + wartoscPoprzednia) / 2;
+                wartoscPoprzednia = wektorPrzyspieszenia;
+
+                //stepCount++;
+
+                if (delta > 1.5)
+                {
+                    stepCount++;
+                }
+
+
+                LabelX.Text = "Twoje polozenie X: " + e.Reading.Acceleration.X.ToString();
+                LabelY.Text = "Twoje polozenie Y: " + e.Reading.Acceleration.Y.ToString();
+                LabelZ.Text = "Twoje polozenie Z: " + e.Reading.Acceleration.Z.ToString();
+                LabelWynik.Text = "Liczba twoich krokow: " + stepCount.ToString();
+
+                //await Task.Delay(5000);
+
+                /*LabelX.Text = e.Reading.Acceleration.X.ToString();
+                LabelY.Text = e.Reading.Acceleration.Y.ToString();
+                LabelZ.Text = e.Reading.Acceleration.Z.ToString();*/
+            //}
         }
 
-        private void stop_Clicked(object sender, EventArgs e)
+        private void Stop_Clicked(object sender, EventArgs e)
         {
             if (!Accelerometer.IsMonitoring)
                 return;
